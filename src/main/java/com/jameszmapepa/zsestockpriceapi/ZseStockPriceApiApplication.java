1df224b61d2802caf023c42cfb4f33da28e93afd
james.mapepa <james.mapepa@cassavasmartech.co.zw>
2021-04-19 10:49:20 +0200

    Initial commit

---
+++ src/main/java/com/jameszmapepa/zsestockpriceapi/ZseStockPriceApiApplication.java
@@ -0,0 +1,13 @@
+package com.jameszmapepa.zsestockpriceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZseStockPriceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZseStockPriceApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jameszmapepa/zsestockpriceapi/ZseStockPriceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jameszmapepa.zsestockpriceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZseStockPriceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
