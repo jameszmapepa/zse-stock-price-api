81124b62b3341e1fd8753e0f097ee7f2db689a8b
james.mapepa <james.mapepa@cassavasmartech.co.zw>
2021-04-21 20:53:22 +0200

    Add basic auth interceptor

--- src/main/java/com/jameszmapepa/zsestockpriceapi/config/BasicAuthInterceptor.java
+++ src/main/java/com/jameszmapepa/zsestockpriceapi/config/BasicAuthInterceptor.java
@@ -2,21 +2,37 @@
 package com.jameszmapepa.zsestockpriceapi.config;
 
 import kotlin.jvm.internal.Intrinsics;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.Credentials;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Request.Builder;
 import okhttp3.Response;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.io.IOException;
 
+@Slf4j
 final class BasicAuthInterceptor implements Interceptor {
+
+    private static final String AUTHORIZATION = "Authorization";
+
+    @Value("${rest.basic-auth-username}")
+    private String username;
+    @Value("${rest.basic-auth-password}")
+    private String password;
+
 
     @NotNull
     public Response intercept(@NotNull Chain chain) throws IOException {
         Intrinsics.checkParameterIsNotNull(chain, "chain");
         Request originalRequest = chain.request();
-        Builder requestBuilder = originalRequest.newBuilder().header("Authorization", "AuthToken");
+
+        log.debug("Authenticating for request: {}", originalRequest);
+        String credential = Credentials.basic(username, password);
+        Builder requestBuilder = originalRequest.newBuilder()
+                .header(AUTHORIZATION, credential);
         Request request = requestBuilder.build();
         return chain.proceed(request);
     }
--- src/main/java/com/jameszmapepa/zsestockpriceapi/config/RestConfig.java
+++ src/main/java/com/jameszmapepa/zsestockpriceapi/config/RestConfig.java
@@ -1,55 +1,30 @@
 package com.jameszmapepa.zsestockpriceapi.config;
 
 import lombok.extern.slf4j.Slf4j;
-import okhttp3.*;
-import org.jetbrains.annotations.NotNull;
+import okhttp3.OkHttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.IOException;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Configuration
 public class RestConfig {
 
-    private List<Interceptor> interceptors;
 
     @Bean
     public OkHttpClient okHttpClient() {
         return new OkHttpClient.Builder()
+                .addInterceptor(new HeaderInterceptor())
+                .addInterceptor(new BasicAuthInterceptor())
                 .addInterceptor(new ErrorInterceptor())
                 .addInterceptor(new LoggingInterceptor())
-                .addInterceptor(new BasicAuthInterceptor())
                 .connectTimeout(10, TimeUnit.SECONDS)
                 .writeTimeout(10, TimeUnit.SECONDS)
                 .readTimeout(30, TimeUnit.SECONDS)
-                .authenticator(getAuthenticator())
+                // .authenticator(getAuthenticator())
                 .build();
     }
 
-    private List<Interceptor> getInterceptors() {
-        interceptors.add(new LoggingInterceptor());
-        interceptors.add(new ErrorInterceptor());
-        return interceptors;
-    }
 
-    private Authenticator getAuthenticator() {
-        return new Authenticator() {
-            @Override
-            public Request authenticate(Route route, @NotNull Response response) throws IOException {
-                if (response.request().header("Authorization") != null) {
-                    return null; // Give up, we've already attempted to authenticate.
-                }
-
-                log.info("Authenticating for response: {}", response);
-                log.info("Challenges: {}", response.challenges());
-                String credential = Credentials.basic("user", "password");
-                return response.request().newBuilder()
-                        .header("Authorization", credential)
-                        .build();
-            }
-        };
-    }
 }
